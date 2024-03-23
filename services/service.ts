f337bd9c2675372f1742c046be8dbcea40bfc2de
Ganesh <gp7.official@gmail.com>
2024-03-23 11:47:10 +0530

    Update service.ts

--- services/service.ts
+++ services/service.ts
@@ -2,7 +2,7 @@ export const getStockData = async (symbol: string) => {
   const response = await fetch(
     `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${
       symbol === "" ? "IBM" : symbol
-    }&outputsize=full&apikey=${process.env.NEXT_PUBLIC_API_KEY}`
+    }&outputsize=full&apikey=demo`
   );
   const data = await response.json();
   return data;
@@ -12,7 +12,7 @@ export const getSMAPoints = async (symbol: string) => {
   const response = await fetch(
     `https://www.alphavantage.co/query?function=SMA&symbol=${
       symbol === "" ? "IBM" : symbol
-    }&interval=daily&time_period=10&series_type=close&apikey=${process.env.NEXT_PUBLIC_API_KEY}`
+    }&interval=daily&time_period=10&series_type=close&apikey=demo`
   );
   const data = await response.json();
   return data;
@@ -22,7 +22,7 @@ export const getRSIPoints = async (symbol: string) => {
   const response = await fetch(
     `https://www.alphavantage.co/query?function=RSI&symbol=${
       symbol === "" ? "IBM" : symbol
-    }&interval=weekly&time_period=10&series_type=open&apikey=${process.env.NEXT_PUBLIC_API_KEY}`
+    }&interval=weekly&time_period=10&series_type=open&apikey=demo`
   );
   const data = await response.json();
   return data;
@@ -32,7 +32,7 @@ export const getADXPoints = async (symbol: string) => {
   const response = await fetch(
     `https://www.alphavantage.co/query?function=ADX&symbol=${
       symbol === "" ? "IBM" : symbol
-    }&interval=daily&time_period=10&apikey=${process.env.NEXT_PUBLIC_API_KEY}`
+    }&interval=daily&time_period=10&apikey=demo`
   );
   const data = await response.json();
   return data;
@@ -42,7 +42,7 @@ export const getAROONPoints = async (symbol: string) => {
   const response = await fetch(
     `https://www.alphavantage.co/query?function=AROON&symbol=${
       symbol === "" ? "IBM" : symbol
-    }&interval=daily&time_period=14&apikey=${process.env.NEXT_PUBLIC_API_KEY}`
+    }&interval=daily&time_period=14&apikey=demo`
   );
   const data = await response.json();
   return data;
